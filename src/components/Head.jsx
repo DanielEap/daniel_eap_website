47cbbfb878c22db2fd30a106c524d8f5c95d2e06
DanielEap <danieliseap@gmail.com>
2023-10-12 23:33:20 -0400

    Revert "added to maybe help icon"
    This reverts commit 96eda6e26e6a31a0bb9440b68154197c9b156050.

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,14 +1,8 @@
 import React, { useEffect } from 'react';
-import icon from '../assets/icon.ico';
+
 function Head() {
   useEffect(() => {
     document.title = 'Daniel Eap';
   }, []);
-
-  return (
-    <head>
-      <link rel="icon" href={icon} />
-    </head>
-  );
 }
 export default Head;
