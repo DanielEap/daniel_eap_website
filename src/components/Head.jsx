96eda6e26e6a31a0bb9440b68154197c9b156050
DanielEap <danieliseap@gmail.com>
2023-10-12 23:30:46 -0400

    added to maybe help icon

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
-
+import icon from '../assets/icon.ico';
 function Head() {
   useEffect(() => {
     document.title = 'Daniel Eap';
   }, []);
+
+  return (
+    <head>
+      <link rel="icon" href={icon} />
+    </head>
+  );
 }
 export default Head;
