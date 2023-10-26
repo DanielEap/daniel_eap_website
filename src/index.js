a6047e2b1ec490fb626a91d82905c6a569892998
DanielEap <danieliseap@gmail.com>
2023-10-26 11:50:59 -0400

    added google analytics

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('G-27M3ECWMX2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.event({
+  category: 'User',
+  action: 'Visited Website',
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
